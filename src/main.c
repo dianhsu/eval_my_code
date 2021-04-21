42f90dbf6c826366129ab2f4d41a68ca816425c2
dianhsu <xudian.cn@gmail.com>
2021-04-21 15:49:17 +0800

    update

--- src/main.c
+++ src/main.c
@@ -5,8 +5,6 @@
 
 
 int main(int argc, char *argv[]) {
-
-
 	struct config _config;
 	struct result _result = {0, 0, 0, 0, 0, 0, 0};
 	_config.exe_path = argv[1];
@@ -32,22 +30,21 @@ int main(int argc, char *argv[]) {
     _config.args[0] = _config.exe_path;
     _config.args[1] = NULL;
     _config.env[0] = NULL;
-    _config.log_path = "judger.log";
+    _config.log_path = "runner.log";
     _config.seccomp_rule_name = NULL;
     _config.uid = 65534;
     _config.gid = 65534;
 
     run(&_config, &_result);
 
-    printf("{\n"
-           "    \"cpu_time\": %d,\n"
-           "    \"real_time\": %d,\n"
-           "    \"memory\": %ld,\n"
-           "    \"signal\": %d,\n"
-           "    \"exit_code\": %d,\n"
-           "    \"error\": %d,\n"
-           "    \"result\": %d\n"
-           "}",
+    printf(
+           "cpu_time: %d ms\n"
+           "real_time: %d ms\n"
+           "memory: %ld bytes\n"
+           "signal: %d\n"
+           "exit_code: %d\n"
+           "error: %d\n"
+           "result: %d\n"
            _result.cpu_time,
            _result.real_time,
            _result.memory,
